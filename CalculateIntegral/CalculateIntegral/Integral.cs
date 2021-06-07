61aceb13337c5c8fe601a6b60bf12fb6937d6f53
BEE063 <antonova.hellokitty2011@yandex.ru>
2021-06-07 22:42:51 +0300

    add integral project

---
+++ CalculateIntegral/CalculateIntegral/Integral.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace CalculateIntegral
+{
+    class Integral
+    {
+        private Func<double, double> func;
+
+        private double _leftPoint;
+        private double _rightPoint;
+        private double _eps;
+        private Semaphore _semaphore;
+
+        public double result { get; set; }
+
+        public Integral(Func<double, double> f, double a, double b, double e)
+        {
+            func = f;
+            _semaphore = new Semaphore(1, 1);
+            _leftPoint = a;
+            _rightPoint = b;
+            _eps = e;
+        }
+
+        private void CalculateIntegral(object args)
+        {
+            _semaphore.WaitOne();
+
+            var left = (double)((object[])args)[0];
+            var right = (double)((object[])args)[1];
+
+            var length = right - left;
+
+            var h = length / 2;
+            var middle = left + h;
+
+            if (length < _eps)
+            {
+                result += func(middle) * length;
+            }
+            else
+            {
+                var threadLeft = new Thread(CalculateIntegral);
+                threadLeft.Start(new object[] { left, middle });
+
+                var threadRight = new Thread(CalculateIntegral);
+                threadRight.Start(new object[] { middle, right });
+            }
+
+            _semaphore.Release();
+        }
+        public void StartCalculate()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var thread = new Thread(CalculateIntegral);
+            thread.Start(new object[] { _leftPoint, _rightPoint });
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("Время вычисления в многопоточном режиме: " + elapsedMs);
+        }
+    }
+}
---
+++ CalculateIntegral/CalculateIntegral/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace CalculateIntegral
+{
+    class Program
+    {
+        public delegate double Function(double x);
+        static void Main(string[] args)
+        {
+            double a = 1;
+            double b = 2;
+
+            var integral = new Integral( f, a, b, 0.001);
+
+            integral.StartCalculate();
+
+            Thread.Sleep(3000);
+            Console.WriteLine($"Результат: {integral.result}");
+
+            
+            int n = 1000;
+            double[] y = new double[n];
+            double h = (b - a) / (n - 1);
+            for (int i = 0; i < n; i++)
+            {
+                double x = i * h;
+                y[i] = Math.Cos(x);
+            }
+            var result = Trapezoidal(f, a, b, n);
+            Console.WriteLine("Результат: {0}", result);
+
+            Console.ReadKey();
+        }
+        static double f(double x)
+        {
+            return Math.Cos(x);
+        }
+        public static double Trapezoidal(Function f, double a, double b, int n)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            double sum = 0;
+            double h = (b - a) / n;
+            for (int i = 0; i < n; i++)
+            {
+                sum += 0.5 * h * (f(a + i * h) + f(a + (i + 1) * h));
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("Время вычисления в однопоточном режиме: " + elapsedMs);
+            return sum;
+        }
+    }
+}
+
